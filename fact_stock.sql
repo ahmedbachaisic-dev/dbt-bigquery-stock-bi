6fa0ad0ce9b1f38c8226ddd49cf97921420680c8
ahmedbachaisic-dev <ahmed.bacha.isic@gmail.com>
2026-01-09 20:52:58 +0100

    Update fact_stock.sql

--- fact_stock.sql
+++ fact_stock.sql
@@ -11,7 +11,6 @@ WITH inventory_transactions AS (
         customer_order_id,
         comments
     FROM {{ ref('stg_inventory_transaction') }}
-    -- PAS de WHERE : on prend TOUTES les transactions
 ),
 
 dim_p AS (
@@ -20,33 +19,35 @@ dim_p AS (
 ),
 
 dim_d AS (
-    SELECT full_date, id_date AS date_key
+    SELECT full_date, id_date
     FROM {{ ref('dim_date') }}
 )
+
 SELECT
     -- KEYS
-    ---------------------------------------------------
     it.inventory_transaction_id AS fact_stock_id,
     dim_p.product_key,
-    -- DATE KEYS
-    dim_d_trans.date_key AS transaction_date_key,
+    dim_d.id_date,                 -- 🔑 clé date pour Power BI
+
     -- MEASURES
     it.quantity,
+
     -- ATTRIBUTES
     it.transaction_type,
     it.comments,
     it.transaction_created_date,
     it.purchase_order_id,
     it.customer_order_id,
+
     -- CALCULATED
     CASE 
-        WHEN it.transaction_type IN (1, 2, 3) THEN 'IN'  -- Types d'entrée
-        WHEN it.transaction_type IN (4, 5) THEN 'OUT'    -- Types de sortie
+        WHEN it.transaction_type IN (1, 2, 3) THEN 'IN'
+        WHEN it.transaction_type IN (4, 5) THEN 'OUT'
         ELSE 'OTHER'
     END AS movement_direction
 
 FROM inventory_transactions it
 LEFT JOIN dim_p
     ON it.product_id = dim_p.product_bk
-LEFT JOIN dim_d AS dim_d_trans
-    ON DATE(it.transaction_created_date) = dim_d_trans.full_date
+LEFT JOIN dim_d
+    ON DATE(it.transaction_created_date) = dim_d.full_date
